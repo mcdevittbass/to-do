bcc162ec8c409802a7b54685b09bb4050648bbd3
Megan McD <mcdevittbass@gmail.com>
2020-08-17 14:26:01 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Main from './components/Main';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <React.Fragment>
+    <HashRouter>
         <Main />
-    </React.Fragment>
+    </HashRouter>
+
   );
 }
 
